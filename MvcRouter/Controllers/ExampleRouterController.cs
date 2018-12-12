b4d26d2b4e40992aad0d857d67ec852f839417c5
Samed Karakus <karakusnavy@gmail.com>
2018-12-12 13:44:38 +0300

    updated

---
+++ MvcRouter/Controllers/ExampleRouterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcRouter.Controllers
+{
+    public class ExampleRouterController : Controller
+    {
+        // GET: ExampleRouter
+        public ActionResult select(int id,string name,string surname)
+        {
+            return Content("Your Id: "+id+"<br />"+"Your Name: "+name+"<br />"+"Your Surname: "+surname);
+        }
+    }
+}
